107cde52badeffd2742697e2c4762c8f702db35a
jreleaserbot <jreleaser@kordamp.org>
2023-02-06 20:30:29 +0000

    jarviz v0.2.0

--- Formula/jarviz.rb
+++ Formula/jarviz.rb
@@ -1,25 +1,29 @@
-# Generated with JReleaser 1.3.1 at 2022-12-18T17:17:36.243322061Z
+# Generated with JReleaser 1.5.0-SNAPSHOT at 2023-02-06T20:28:32.478135496Z
 class Jarviz < Formula
   desc "JAR file analyzer"
   homepage "https://github.com/kordamp/jarviz"
-  version "0.1.0"
+  version "0.2.0"
   license "Apache-2.0"
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/kordamp/jarviz/releases/download/v0.1.0/jarviz-standalone-0.1.0-linux-aarch64.zip"
-    sha256 "3f8a0df6a18ab9bda1404757972538325c2d4ed883a98a9d4c437b880248deab"
+    url "https://github.com/kordamp/jarviz/releases/download/v0.2.0/jarviz-standalone-0.2.0-linux-aarch64.zip"
+    sha256 "fe13f1edfabe6f76b831b4de7e7478b8fb9ab3749f5706ff29e154cd2d6b0575"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/kordamp/jarviz/releases/download/v0.1.0/jarviz-standalone-0.1.0-linux-x86_64.zip"
-    sha256 "254963825b2e32e321458561add3404c83e7c71eee1b50815607837edf643e53"
+    url "https://github.com/kordamp/jarviz/releases/download/v0.2.0/jarviz-standalone-0.2.0-linux-x86_32.zip"
+    sha256 "4ee54f8259de070c4cef7ec4d482809b1d97f1096caa606b7200108c0e3544fe"
+  end
+  if OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/kordamp/jarviz/releases/download/v0.2.0/jarviz-standalone-0.2.0-linux-x86_64.zip"
+    sha256 "5de5bd7fb689ac23676e85bbc8e1b348b3d81ac931015a614bfe09e18b1a1591"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/kordamp/jarviz/releases/download/v0.1.0/jarviz-standalone-0.1.0-osx-aarch64.zip"
-    sha256 "8ee2658f1ef6ff18ee87e57dcb721f2434893f58fb67ebe2916f5ebcd474dd34"
+    url "https://github.com/kordamp/jarviz/releases/download/v0.2.0/jarviz-standalone-0.2.0-osx-aarch64.zip"
+    sha256 "bff544c1f7bfbef81a6a2c6a71b4e4fa03a93005a8e7d35d4df91dd9b49a045a"
   end
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/kordamp/jarviz/releases/download/v0.1.0/jarviz-standalone-0.1.0-osx-x86_64.zip"
-    sha256 "e25d8890050d18bd440cc507a569b0574ea60c94153bc10a4000e8ff248c58ab"
+    url "https://github.com/kordamp/jarviz/releases/download/v0.2.0/jarviz-standalone-0.2.0-osx-x86_64.zip"
+    sha256 "d924af8a88375222e79553ecb9a2213f8f9e9545a6f1a1e74dfd9ddf71c6a605"
   end
 
 
@@ -40,6 +44,6 @@ class Jarviz < Formula
 
   test do
     output = shell_output("#{bin}/jarviz --version")
-    assert_match "0.1.0", output
+    assert_match "0.2.0", output
   end
 end
