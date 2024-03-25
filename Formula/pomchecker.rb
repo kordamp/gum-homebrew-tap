35c1d7e9484afacc97006343df72bb78a603ebce
jreleaserbot <jreleaser@kordamp.org>
2024-03-25 18:04:52 +0000

    pomchecker v1.11.0

--- Formula/pomchecker.rb
+++ Formula/pomchecker.rb
@@ -1,10 +1,11 @@
-# Generated with JReleaser 1.9.0 at 2023-11-30T13:47:55.227517669Z
+# Generated with JReleaser 1.11.0 at 2024-03-25T18:01:40.230608884Z
+
 class Pomchecker < Formula
   desc "Checks POM files may be uploaded to Maven Central"
   homepage "https://kordamp.org/pomchecker"
-  url "https://github.com/kordamp/pomchecker/releases/download/v1.10.0/pomchecker-1.10.0.zip"
-  version "1.10.0"
-  sha256 "966635d0ec63b68f074a8e11710ba0ea25431e030b760c7f609dbce56bb2c27f"
+  url "https://github.com/kordamp/pomchecker/releases/download/v1.11.0/pomchecker-1.11.0.zip"
+  version "1.11.0"
+  sha256 "1676c2a70f3a84b908be36abda70107dfd72e747e86bf337cebd99d9dd082f3f"
   license "Apache-2.0"
 
   depends_on "openjdk@8"
@@ -16,6 +17,6 @@ class Pomchecker < Formula
 
   test do
     output = shell_output("#{bin}/pomchecker --version")
-    assert_match "1.10.0", output
+    assert_match "1.11.0", output
   end
 end
